7015325743b121e628b53d990fe907dd05215041
wasd <wasdsweden@gmail.com>
2017-09-07 22:19:12 +0200

    Added boilerplate code

---
+++ main/java/org/lwjglb/engine/GameEngine.java
@@ -0,0 +1,99 @@
+package org.lwjglb.engine;
+
+public class GameEngine implements Runnable {
+
+    public static final int TARGET_FPS = 60;
+
+    public static final float LOOP_SLOT = 1f / TARGET_FPS;
+
+    private final Window window;
+
+    private final Thread gameLoopThread;
+
+    private final Timer timer;
+
+    private final IGameLogic gameLogic;
+
+    private final MouseInput mouseInput;
+
+    public GameEngine(String windowTitle, int width, int height, boolean vSync, IGameLogic gameLogic) throws Exception {
+        gameLoopThread = new Thread(this, "GAME_LOOP_THREAD");
+        window = new Window(windowTitle, width, height, vSync);
+        mouseInput = new MouseInput();
+        this.gameLogic = gameLogic;
+        timer = new Timer();
+    }
+
+    public void start() {
+        String osName = System.getProperty("os.name");
+        if (osName.contains("Mac")) {
+            gameLoopThread.run();
+        } else {
+            gameLoopThread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            init();
+            gameLoop();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+        } finally {
+            cleanup();
+        }
+    }
+
+    protected void init() throws Exception {
+        window.init();
+        timer.init();
+        mouseInput.init(window);
+        gameLogic.init(window);
+    }
+
+    protected void gameLoop() {
+        boolean running = true;
+        while (running && !window.windowShouldClose()) {
+//            float elapsedTime = timer.getElapsedTime();
+
+            input();
+
+            update(LOOP_SLOT);
+
+            render();
+
+            if (!window.isvSync()) {
+                sync();
+            }
+        }
+    }
+
+    protected void cleanup() {
+        gameLogic.cleanup();
+    }
+
+    private void sync() {
+        double endTime = timer.getLastLoopTime() + LOOP_SLOT;
+        while (timer.getTime() < endTime) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ie) {
+            }
+        }
+    }
+
+    protected void input() {
+        mouseInput.input(window);
+        gameLogic.input(window, mouseInput);
+    }
+
+    protected void update(float interval) {
+        gameLogic.update(interval, mouseInput);
+    }
+
+    protected void render() {
+        gameLogic.render(window);
+        window.update();
+    }
+}
